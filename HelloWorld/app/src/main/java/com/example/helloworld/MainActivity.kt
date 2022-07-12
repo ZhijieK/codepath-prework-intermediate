b3261a7a60e0f924ed2ebf1017cda99162d9de1e
Zhijie Kuang <zhijiekuangpersonal@gmail.com>
2022-07-11 21:37:22 -0400

    Added additional features and updated README

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -5,18 +5,48 @@ import android.os.Bundle
 import android.util.Log
 import android.util.Log.i
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.helloButton)
-        button.setOnClickListener {
+        val bg = findViewById<ConstraintLayout>(R.id.background)
+        val info = findViewById<TextView>(R.id.textView)
+        val catordog = findViewById<TextView>(R.id.question)
+        val helloButton = findViewById<Button>(R.id.helloButton)
+        val catButton = findViewById<Button>(R.id.cats)
+        val dogButton = findViewById<Button>(R.id.dogs)
+        val lightmode = findViewById<Button>(R.id.light)
+        val darkmode = findViewById<Button>(R.id.dark)
+        //Hello Button Response
+        helloButton.setOnClickListener {
             //Button Test
             Log.v("Hello world", "Button clicked!")
             // Hello Response
             Toast.makeText(this, "Hello! Hope you are doing well!", Toast.LENGTH_SHORT).show()
+        }
+        //Cat Button Response
+        catButton.setOnClickListener {
+            Toast.makeText(this,"Hello my fellow cat lover!", Toast.LENGTH_SHORT).show()
+        }
+        //Dog Button Response
+        dogButton.setOnClickListener {
+            Toast.makeText(this, "Dogs are adorable!", Toast.LENGTH_SHORT).show()
+        }
+        //Light mode button
+        lightmode.setOnClickListener {
+            bg.setBackgroundColor(getResources().getColor(R.color.white))
+            info.setTextColor(getResources().getColor(R.color.black))
+            catordog.setTextColor(getResources().getColor(R.color.black))
+        }
+        //Dark mode button
+        darkmode.setOnClickListener {
+            bg.setBackgroundColor(getResources().getColor(R.color.black))
+            info.setTextColor(getResources().getColor(R.color.white))
+            catordog.setTextColor(getResources().getColor(R.color.white))
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# codepath-prework-intermediate
-Codepath Android intermediate prework project
+# Android Prework - *HelloWorld*
+
+Submitted by: **Zhijie Kuang**
+
+**HelloWorld** is an android app that shows an image and introductory message, and allows pressing a button to display a Toast. 
+
+Time spent: **2** hours spent in total
+
+## Required Features
+
+The following **required** functionality is completed:
+
+* [x] Image and introductory message displayed on screen
+* [x] Button displayed on screen
+* [x] Toast with message appears when button is pressed 
+
+The following **optional** features are implemented:
+
+* [ ] User can **tap a todo item in the list and bring up an edit screen for the todo item** and then have any changes to the text reflected in the todo list
+
+The following **additional** features are implemented:
+
+* [x] Added a question and button option for dog lovers and cat lovers.
+* [x] Added additional images to correspond with the choices above.
+* [x] Added light and dark modes for user preferences.
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='http://g.recordit.co/p1PQF4aNP4.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+## Notes
+
+Describe any challenges encountered while building the app.
+
+The only challenge that I encountered was aligning the buttons properly since I am still learning how to utilize the constraints.
+
+## License
+
+    Copyright [yyyy] [name of copyright owner]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
